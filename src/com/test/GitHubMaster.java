6cc98c4f6963d2ad630a9ae60789cd414452462b
viveksha16 <viveks161295@gmail.com>
2022-11-07 16:42:34 +0530

    John Updating Login function

--- src/com/test/GitHubMaster.java
+++ src/com/test/GitHubMaster.java
@@ -13,5 +13,17 @@ public class GitHubMaster {
 		// TODO Auto-generated method stub
 
 	}
+	private void method4() {
+		// TODO Auto-generated method stub
+
+	}
+	private void method5() {
+		// TODO Auto-generated method stub
+
+	}
+	private void method6() {
+		// TODO Auto-generated method stub
+
+	}
 
 }
